18a8d5c4099dc71964a486176313944d94014103
zach <zach@Zachs-MBP.attlocal.net>
2020-05-21 09:08:08 -0400

    updated content block templates; homepage template; footer added and using footer nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ const plugin = require('tailwindcss/plugin');
 
 module.exports = {
   theme: {
-    extend: {}
+    fontFamily:{},
+    extend: {
+      colors: {
+        darkPurple:'#2C2A4A',
+        lightPurple:'#907AD6',
+        lightBlue:'#7FDEFF'
+      }
+    }
   },
   variants: {},
   plugins: [
